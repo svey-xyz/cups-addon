799761213732c41c8b6a9a16b8049586b95dfa59
Andrea Restello <andrea.restello@gmail.com>
2025-05-08 17:53:25 +0200

    Move everything in a subfolder and add repository yaml

--- cups/rootfs/etc/cont-init.d/cups.sh
+++ cups/rootfs/etc/cont-init.d/cups.sh
@@ -36,6 +36,23 @@ Listen 0.0.0.0:631
   Allow 192.168.0.0/16
 </Location>
 
+# Job management permissions
+<Location /jobs>
+  Order allow,deny
+  Allow localhost
+  Allow 10.0.0.0/8
+  Allow 172.16.0.0/12
+  Allow 192.168.0.0/16
+</Location>
+
+<Limit Send-Document Send-URI Hold-Job Release-Job Restart-Job Purge-Jobs Set-Job-Attributes Create-Job-Subscription Renew-Subscription Cancel-Subscription Get-Notifications Reprocess-Job Cancel-Current-Job Suspend-Current-Job Resume-Job Cancel-My-Jobs Close-Job CUPS-Move-Job CUPS-Get-Document>
+  Order allow,deny
+  Allow localhost
+  Allow 10.0.0.0/8
+  Allow 172.16.0.0/12
+  Allow 192.168.0.0/16
+</Limit>
+
 # Enable web interface
 WebInterface Yes
 
